ab611c7a34dd2929a20c2e199197373d3013d984
Yuwei <48247468+Yuwei-Econ@users.noreply.github.com>
2019-03-26 19:11:14 +0800

    Update homework2.R

--- homework2.R
+++ homework2.R
@@ -1,1 +1,7 @@
 NULL
+WageTest <- subset(Wage, Wage["age"]<34 & Wage["age"]>28)
+Testfit<- lm(logwage ~ age, data=WageTest)
+coeftest(Testfit)
+plot(logwage~ age, data=WageTest)
+abline(lm(logwage~age,data=WageTest),col="green")
+ggplot(WageTest, aes(x=age, y=logwage)) + geom_point()+stat_smooth(se=F, method='lm', formula=y~poly(x,4))
